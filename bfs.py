59789ca9a0e14ccb2bc45a5bc2c6aa2d644c1071
elioti <34484940+elioti@users.noreply.github.com>
2019-05-01 20:25:24 +0800

    modification bfs

--- bfs.py
+++ bfs.py
@@ -7,21 +7,26 @@ Output: true
 def isInterleave(s1, s2, s3):
     len_s1 = len(s1)
     len_s2 = len(s2)
-    ans = False
+    if len_s1 +len_s2 != len(s3):
+        return False
+    dict1 = {}
     def partition(i, j):
-        global ans
+        tmp = dict1.get((i,j), None)
+        if tmp is not None:
+            return tmp
         if i == len_s1 and j == len_s2:
-            print('aa')
-            ans = True
-            print(ans)
-            return
+            dict1[(i, j)] = True
+            return True
         else:
+            a, b = False, False
             if i < len_s1 and s1[i] == s3[i+j]:
-                partition(i+1, j)
+                a = partition(i+1, j)
             if j < len_s2 and s2[j] == s3[i+j]:
-                partition(i, j+1)
-            else:
-                return
-    partition(0,0)
-    print(ans)
-isInterleave('aabcc', 'dbbca', 'aadbbcbcac')
+                b = partition(i, j+1)
+            dict1[(i,j)] = a or b
+            return a or b
+    ans = partition(0,0)
+    print(dict1)
+    return ans
+print(isInterleave('abbbbbbcabbacaacccababaabcccabcacbcaabbbacccaaaaaababbbacbb', 'ccaacabbacaccacababbbbabbcacccacccccaabaababacbbacabbbbabc', 'cacbabbacbbbabcbaacbbaccacaacaacccabababbbababcccbabcabbaccabcccacccaabbcbcaccccaaaaabaaaaababbbbacbbabacbbacabbbbabc'))
+# print(isInterleave('aabcc','dbbca','aadbbbaccc'))
